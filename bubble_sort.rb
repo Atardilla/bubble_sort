bb49c9a1a6c16a408c34ea1fc8ec523b36eaef4e
Bambi <61665527+Atardilla@users.noreply.github.com>
2021-01-27 14:17:11 -0300

    Create bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,16 @@
+initial_array = [3, 1, 8, 7, 2, 5, 4, 6]
+
+def bubble_sort(array)
+  for y in (0...array.length)
+    for i in (0...array.length-1)
+        if array[i] > array[i+1]
+          array[i], array[i+1] = array[i+1], array[i]
+          print array;puts
+        end
+    end
+  end
+  return array;puts
+end
+
+print initial_array;puts
+print bubble_sort(initial_array);puts
